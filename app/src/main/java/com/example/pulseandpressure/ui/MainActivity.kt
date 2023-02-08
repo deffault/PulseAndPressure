d64ce6403f3e3b77e8eb9bbe548133965c0be18c
Dmitriy Negrobov <dv.negrobov@gmail.com>
2023-02-08 21:46:33 +0300

    add packages

--- app/src/main/java/com/example/pulseandpressure/ui/MainActivity.kt
+++ app/src/main/java/com/example/pulseandpressure/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.pulseandpressure
+package com.example.pulseandpressure.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pulseandpressure.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
